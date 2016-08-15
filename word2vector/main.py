15677b94734e0ab6ae397f0a990de5e2e9bf6d85
kangkang <1522847010@qq.com>
2016-08-15 09:45:37 +0800

    得到词典
    在将句子分好词的前提下，得到词典，可以由这个词典形成词袋模型。

--- word2vector/main.py
+++ word2vector/main.py
@@ -14,6 +14,12 @@ from MyCorpus import *
 
 
 if __name__ == "__main__":
-     demo = Read_Comment()
+     # 输入需要处理的文本
+     demo = Read_Comment('phone_comment_content2.txt')
+
+     # 对文本分词
      my_corpus = MyCorpus(demo)
+
+     # 得到字典
+     # 此处必须先执行一次，因为字典是遍历完才得到，不能边迭代产生每个单词的词袋模型边使用字典
      my_corpus.get_Corpus()
